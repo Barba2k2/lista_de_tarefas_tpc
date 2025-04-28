43fb7942764ddb7d6bd653a5b9b39d1972e14454
Barba2k2 <lorenzo.dz@hotmail.com>
2025-04-28 19:48:49 -0300

    Initializes Todo app structure and basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/todo/widgtes/todo_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Lista de Tarefas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: false,
       ),
+      home: const TodoScreen(),
     );
   }
 }
