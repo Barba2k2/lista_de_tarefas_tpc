77fd4411b823f9f3cdbee7ce735d320fd13b8ff7
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-29 16:47:49 -0300

    Adicionar modelo TodoApiModel com suporte a criação e atualização de tarefas, utilizando Freezed e JsonSerializable para serialização JSON.

---
+++ lib/data/services/models/todo/todo_api_model.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo_api_model.freezed.dart';
+part 'todo_api_model.g.dart';
+
+@freezed
+abstract class TodoApiModel with _$TodoApiModel {
+  const factory TodoApiModel.create({
+    required String name, //
+  }) = CreateTodoApiModel;
+
+  const factory TodoApiModel.update({
+    required String id,
+    required String name,
+  }) = UpdateTodoApiModel;
+
+  factory TodoApiModel.fromJson(Map<String, Object?> json) =>
+      _$TodoApiModelFromJson(json);
+}
