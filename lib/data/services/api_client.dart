60d38b073fb8635b89c0069541d41c38669a3048
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-29 14:44:30 -0300

    Adicionar método postTodo ao ApiClient para criar novos todos no servidor

--- lib/data/services/api_client.dart
+++ lib/data/services/api_client.dart
@@ -44,4 +44,38 @@ class ApiClient {
       client.close();
     }
   }
+
+  Future<Result<Todo>> postTodo(Todo todo) async {
+    final client = _clientHttpFactory();
+
+    try {
+      final request = await client.post(_host, _port, '/todos');
+
+      request.write(
+        jsonEncode(
+          {
+            'name': todo.name, //
+          },
+        ),
+      );
+
+      final response = await request.close();
+
+      if (response.statusCode == HttpStatus.created) {
+        final stringData = await response.transform(utf8.decoder).join();
+
+        final json = jsonDecode(stringData) as Map<String, dynamic>;
+
+        final createdTodo = Todo.fromJson(json);
+
+        return Result.ok(createdTodo);
+      } else {
+        return Result.error(
+          const HttpException('Invalid response'), //
+        );
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
 }
