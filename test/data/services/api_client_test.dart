9c24b6bb85263015b12752729ce96af5b1b1698c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-29 16:48:20 -0300

    Adicionar teste unitário para o método updateTodo() no ApiClient, garantindo que a atualização de um Todo funcione corretamente.

--- test/data/services/api_client_test.dart
+++ test/data/services/api_client_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lista_de_tarefas_tpc/data/services/api_client.dart';
+import 'package:lista_de_tarefas_tpc/data/services/models/todo/todo_api_model.dart';
 import 'package:lista_de_tarefas_tpc/domain/models/todo.dart';
 import 'package:lista_de_tarefas_tpc/utils/result/result.dart';
 
@@ -18,7 +19,7 @@ void main() {
     });
 
     test('Should return Result Ok when postTodo()', () async {
-      final todoToCreate = Todo(name: 'Todo created on TEST');
+      const todoToCreate = CreateTodoApiModel(name: 'Todo created on TEST');
 
       final result = await apiClient.postTodo(todoToCreate);
 
@@ -26,14 +27,28 @@ void main() {
     });
 
     test('Should return Result Ok when deleteTodo()', () async {
-      final todoToCreate = Todo(name: 'Todo created on TEST');
+      const todoToCreate = CreateTodoApiModel(name: 'Todo created on TEST');
 
       final createdTodoResult = await apiClient.postTodo(todoToCreate);
 
       final result = await apiClient.deleteTodo(createdTodoResult.asOk.value);
 
       expect(result.asOk, isA<Result<void>>());
-      
+    });
+
+    test('Should update Todo when deleteTodo()', () async {
+      const todoToCreate = CreateTodoApiModel(name: 'Todo created on TEST');
+
+      final createdTodoResult = await apiClient.postTodo(todoToCreate);
+
+      final result = await apiClient.updateTodo(
+        UpdateTodoApiModel(
+          id: createdTodoResult.asOk.value.id!,
+          name: "${createdTodoResult.asOk.value.name} updatedDate ${DateTime.now().toIso8601String()}",
+        ),
+      );
+
+      expect(result.asOk, isA<Result<Todo>>());
     });
   });
 }
