74db89c116cee650ad3e8861f6ec7e549a071d92
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-30 13:49:21 -0300

    Adicionar implementação do método updateTodo nas classes TodosRepositoryDev e TodosRepositoryRemote, permitindo a atualização de tarefas existentes e melhorando a flexibilidade na manipulação de dados.

--- lib/data/repository/todos/todos_repository_dev.dart
+++ lib/data/repository/todos/todos_repository_dev.dart
@@ -40,4 +40,13 @@ class TodosRepositoryDev implements TodosRepository {
   Future<Result<Todo>> getById(String id) async {
     return Result.ok(_todos.firstWhere((todo) => todo.id == id));
   }
+  
+  @override
+  Future<Result<Todo>> updateTodo(Todo todo) async {
+    final todoIndex = _todos.indexWhere((t) => t.id == todo.id);
+
+    _todos[todoIndex] = todo;
+
+    return Result.ok(todo);
+  }
 }
