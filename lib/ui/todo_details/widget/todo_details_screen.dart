1c4a6f53e9d3234ac7e8124c7e1144e893fd5d5d
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-30 12:05:20 -0300

    Refatorar a tela TodoDetailsScreen para utilizar ListenableBuilder, melhorando a gestão do estado e a apresentação dos detalhes da tarefa. Remover a propriedade id e integrar o widget TodoNameWidget para exibir o nome da tarefa de forma mais eficiente.

--- lib/ui/todo_details/widget/todo_details_screen.dart
+++ lib/ui/todo_details/widget/todo_details_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../view_models/todos_details_view_model.dart';
+import 'todo_name_widget.dart';
 
 class TodoDetailsScreen extends StatefulWidget {
-  final String id;
   final TodosDetailsViewModel todoDetailsViewModel;
 
   const TodoDetailsScreen({
     super.key,
-    required this.id,
-    required this.todoDetailsViewModel,
+    required this.todoDetailsViewModel, //
   });
 
   @override
@@ -23,8 +22,34 @@ class _TodoDetailsScreenState extends State<TodoDetailsScreen> {
       appBar: AppBar(
         title: const Text('Listando detalhes da Tarefa'), //
       ),
-      body: Center(
-        child: Text('Detalhes da Tarefa: ${widget.id}'), //
+      body: ListenableBuilder(
+        listenable: widget.todoDetailsViewModel,
+        builder: (context, child) {
+          if (widget.todoDetailsViewModel.load.running) {
+            return Center(
+              child: CircularProgressIndicator.adaptive(), //
+            );
+          }
+
+          if (widget.todoDetailsViewModel.load.error) {
+            return Center(
+              child: Text('Ocorreu um erro ao carregar a tarefa'), //
+            );
+          }
+
+          return child!;
+        },
+        child: ListenableBuilder(
+          listenable: widget.todoDetailsViewModel,
+          builder: (context, child) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TodoNameWidget(
+                todo: widget.todoDetailsViewModel.todo, //
+              ),
+            );
+          },
+        ),
       ),
     );
   }
