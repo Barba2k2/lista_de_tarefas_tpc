24aa2f9c91d50965f04ff80221155605cb82b2e7
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-30 09:32:50 -0300

    Adicionar configuração de rotas utilizando GoRouter, definindo a rota inicial para a tela de tarefas e integrando o ViewModel com o repositório remoto.

---
+++ lib/routing/router.dart
@@ -0,0 +1,29 @@
+import 'package:go_router/go_router.dart';
+
+import '../data/repository/todos/todos_repository_remote.dart';
+import '../data/services/api_client.dart';
+import '../ui/todo/viewmodels/todo_view_model.dart';
+import '../ui/todo/widgtes/todo_screen.dart';
+import 'routes.dart';
+
+GoRouter routerConfig() {
+  return GoRouter(
+    initialLocation: Routes.todos,
+    routes: [
+      GoRoute(
+        path: Routes.todos,
+        builder: (context, state) {
+          return TodoScreen(
+            todoViewModel: TodoViewModel(
+              todosRepository: TodosRepositoryRemote(
+                apiClient: ApiClient(
+                  host: "192.168.0.238", //
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    ],
+  );
+}
