691635f9b6ed41cb9c22054daa9204fc0874f720
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-30 12:37:21 -0300

    Atualizar método add na interface TodosRepository para aceitar descrição e status da tarefa, melhorando a flexibilidade na criação de novas tarefas.

--- lib/data/repository/todos/todos_repository.dart
+++ lib/data/repository/todos/todos_repository.dart
@@ -3,7 +3,11 @@ import '../../../domain/models/todo.dart';
 
 abstract class TodosRepository {
   Future<Result<List<Todo>>> get();
-  Future<Result<Todo>> add(String name);
+  Future<Result<Todo>> add({
+    required String name,
+    required String description,
+    required bool done,
+  });
   Future<Result<void>> delete(Todo todo);
   Future<Result<Todo>> getById(String id);
 }
