790c5e6880fa965d9177982cb8193eab8ef2fa36
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-30 11:29:43 -0300

    Refatorar a configuração do GoRouter para utilizar uma instância compartilhada de TodosRepositoryRemote, melhorando a eficiência e a separação de responsabilidades. Adicionar a injeção do TodosDetailsViewModel na rota de detalhes da tarefa, permitindo o carregamento adequado dos dados da tarefa pelo ID.

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -4,10 +4,17 @@ import '../data/repository/todos/todos_repository_remote.dart';
 import '../data/services/api_client.dart';
 import '../ui/todo/viewmodels/todo_view_model.dart';
 import '../ui/todo/widgtes/todo_screen.dart';
+import '../ui/todo_details/view_models/todos_details_view_model.dart';
 import '../ui/todo_details/widget/todo_details_screen.dart';
 import 'routes.dart';
 
 GoRouter routerConfig() {
+  final todosRepository = TodosRepositoryRemote(
+    apiClient: ApiClient(
+      host: "192.168.0.238", //
+    ),
+  );
+
   return GoRouter(
     initialLocation: Routes.todos,
     routes: [
@@ -16,11 +23,7 @@ GoRouter routerConfig() {
         builder: (context, state) {
           return TodoScreen(
             todoViewModel: TodoViewModel(
-              todosRepository: TodosRepositoryRemote(
-                apiClient: ApiClient(
-                  host: "192.168.0.238", //
-                ),
-              ),
+              todosRepository: todosRepository, //
             ),
           );
         },
@@ -29,8 +32,16 @@ GoRouter routerConfig() {
             path: ':id',
             builder: (context, state) {
               final todoId = state.pathParameters['id'] ?? '';
+              final todoDetailsViewModel = TodosDetailsViewModel(
+                todosRepository: todosRepository,
+              );
 
-              return TodoDetailsScreen(id: todoId);
+              todoDetailsViewModel.load.execute(todoId);
+
+              return TodoDetailsScreen(
+                id: todoId,
+                todoDetailsViewModel: todoDetailsViewModel,
+              );
             }, //
           ),
         ],
