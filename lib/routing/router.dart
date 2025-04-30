2a2d582f304327bb50eaf3969cdfc940a7f508f2
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-30 10:04:52 -0300

    Adicionar rota para detalhes da tarefa no GoRouter, permitindo a navegação para a tela de detalhes com base no ID da tarefa.

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -4,6 +4,7 @@ import '../data/repository/todos/todos_repository_remote.dart';
 import '../data/services/api_client.dart';
 import '../ui/todo/viewmodels/todo_view_model.dart';
 import '../ui/todo/widgtes/todo_screen.dart';
+import '../ui/todo_details/widget/todo_details_screen.dart';
 import 'routes.dart';
 
 GoRouter routerConfig() {
@@ -23,6 +24,16 @@ GoRouter routerConfig() {
             ),
           );
         },
+        routes: [
+          GoRoute(
+            path: ':id',
+            builder: (context, state) {
+              final todoId = state.pathParameters['id'] ?? '';
+
+              return TodoDetailsScreen(id: todoId);
+            }, //
+          ),
+        ],
       ),
     ],
   );
