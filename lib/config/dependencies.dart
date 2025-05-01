f103d2b9bb0249610ea9587627870c7b121671cc
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-05-01 18:26:28 -0300

    Remover provedores de dependências remotas do arquivo dependencies.dart, simplificando a configuração e eliminando a lógica de comunicação com a API, em linha com a recente remoção das classes TodosRepositoryRemote e ApiClient.

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -3,30 +3,7 @@ import 'package:provider/single_child_widget.dart';
 
 import '../data/repository/todos/todos_repository.dart';
 import '../data/repository/todos/todos_repository_dev.dart';
-import '../data/repository/todos/todos_repository_remote.dart';
-import '../data/services/api_client.dart';
 import '../domain/use_cases/todo_update_use_case.dart';
-
-List<SingleChildWidget> get providersRemote {
-  return [
-    Provider(
-      create: (context) {
-        return ApiClient(
-          host: '192.168.0.238', //
-        );
-      },
-    ),
-    ChangeNotifierProvider(
-      create: (context) {
-        return TodosRepositoryRemote(
-              apiClient: context.read(),
-            )
-            as TodosRepository;
-      },
-    ),
-    ..._sharedProviders,
-  ];
-}
 
 List<SingleChildWidget> get providersLocal {
   return [
