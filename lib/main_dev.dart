3fff30c3272070bb72a3cc12217dd74471b5366c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-05-01 18:28:49 -0300

    Refatorar a inicialização do aplicativo para incluir provedores de serviços e repositórios locais, melhorando a estrutura de dependências e a organização do código. Adicionar suporte para armazenamento e atualização de tarefas.

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,23 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
-import 'config/dependencies.dart';
+import 'data/repository/todos/todos_repository.dart';
+import 'data/repository/todos/todos_repository_dev.dart';
+import 'data/services/storage_service.dart';
+import 'domain/use_cases/todo_update_use_case.dart';
 import 'main.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((log) {
-    print('[${log.level}] - [${log.loggerName}] - ${log.message}');
-    if(log.stackTrace != null){
-      print(log.stackTrace);
+    debugPrint('[${log.level}] - [${log.loggerName}] - ${log.message}');
+    if (log.stackTrace != null) {
+      debugPrint(log.stackTrace.toString());
     }
   });
 
   runApp(
     MultiProvider(
-      providers: providersRemote,
-      // providers: providersLocal, => For local repository
-      child: const ListaDeTarefasApp(), //
-    ), //
+      providers: [
+        // Serviço de armazenamento
+        Provider<StorageService>(create: (_) => StorageService()),
+
+        // Repositório de tarefas
+        ChangeNotifierProvider<TodosRepository>(
+          create:
+              (context) => TodosRepositoryDev(
+                storageService: context.read<StorageService>(),
+              ),
+        ),
+
+        // Caso de uso para atualização de tarefas
+        Provider<TodoUpdateUseCase>(
+          create:
+              (context) => TodoUpdateUseCase(
+                todosRepository: context.read<TodosRepository>(),
+              ),
+        ),
+      ],
+      child: const ListaDeTarefasApp(),
+    ),
   );
 }
