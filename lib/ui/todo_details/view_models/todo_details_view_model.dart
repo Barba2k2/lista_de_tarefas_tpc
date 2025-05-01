c3fe63bcdc7fa498c00fffffed5f2679521fc504
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-05-01 19:21:46 -0300

    Adicionar logging à classe TodoDetailsViewModel para melhorar a rastreabilidade de erros e eventos. Implementar verificação de ID vazio ao carregar tarefas e garantir a remoção do listener do repositório ao descartar a ViewModel.

--- lib/ui/todo_details/view_models/todo_details_view_model.dart
+++ lib/ui/todo_details/view_models/todo_details_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 import '../../../data/repository/todos/todos_repository.dart';
 import '../../../domain/models/todo.dart';
@@ -9,6 +10,16 @@ import '../../../utils/result/result.dart';
 class TodoDetailsViewModel extends ChangeNotifier {
   final TodosRepository _todosRepository;
   final TodoUpdateUseCase _todoUpdateUseCase;
+  final _logger = Logger('TodoDetailsViewModel');
+
+  Todo _todo = Todo(
+    id: '',
+    name: '',
+    description: '',
+    done: false, //
+  );
+
+  bool _isLoaded = false;
 
   TodoDetailsViewModel({
     required TodosRepository todosRepository,
@@ -17,34 +28,52 @@ class TodoDetailsViewModel extends ChangeNotifier {
        _todoUpdateUseCase = todoUpdateUseCase {
     load = Command1(_load);
     updateTodo = Command1(_todoUpdateUseCase.updateTodo);
-    _todosRepository.addListener(() {
+    _todosRepository.addListener(_onRepositoryChanged);
+  }
+
+  void _onRepositoryChanged() {
+    if (_isLoaded) {
       load.execute(_todo.id);
-    });
+    }
   }
 
   late final Command1<Todo, String> load;
-
   late final Command1<Todo, Todo> updateTodo;
 
-  late Todo _todo;
+  bool get isLoaded => _isLoaded;
 
   Todo get todo => _todo;
 
   Future<Result<Todo>> _load(String id) async {
     try {
+      if (id.isEmpty) {
+        _logger.warning('Tentativa de carregar tarefa com ID vazio');
+        return Result.error(Exception('ID de tarefa vazio'));
+      }
+
       final result = await _todosRepository.getById(id);
 
       switch (result) {
         case Ok<Todo>():
           _todo = result.value;
+          _isLoaded = true;
+          _logger.fine('Tarefa carregada com sucesso: ${_todo.id}');
           return Result.ok(_todo);
         default:
+          _logger.warning('Falha ao carregar tarefa', result.asError.error);
           return result;
       }
-    } on Exception catch (e) {
+    } on Exception catch (e, s) {
+      _logger.severe('Erro ao carregar tarefa', e, s);
       return Result.error(e);
     } finally {
       notifyListeners();
     }
+  }
+
+  @override
+  void dispose() {
+    _todosRepository.removeListener(_onRepositoryChanged);
+    super.dispose();
   }
 }
