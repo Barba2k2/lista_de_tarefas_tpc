88c6457d8b3471eb2e2b6c98c6c567d5296b789a
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-05-01 19:18:57 -0300

    Refatorar a classe TodosRepositoryDev para integrar o StorageService, permitindo o carregamento e salvamento de tarefas de forma persistente. Adicionar tratamento de erros e notificação de mudanças na lista de tarefas.

--- lib/data/repository/todos/todos_repository_dev.dart
+++ lib/data/repository/todos/todos_repository_dev.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 import '../../../utils/result/result.dart';
 import '../../../domain/models/todo.dart';
+import '../../services/storage_service.dart';
 import 'todos_repository.dart';
 
 class TodosRepositoryDev extends ChangeNotifier implements TodosRepository {
-  final List<Todo> _todos = [];
+  final StorageService _storageService;
+  List<Todo> _todos = [];
+
+  TodosRepositoryDev({StorageService? storageService})
+    : _storageService = storageService ?? StorageService() {
+    _loadTodos();
+  }
+
+  Future<void> _loadTodos() async {
+    _todos = await _storageService.loadTodos();
+    notifyListeners();
+  }
 
   @override
   List<Todo> get todos => _todos;
@@ -19,14 +31,18 @@ class TodosRepositoryDev extends ChangeNotifier implements TodosRepository {
     try {
       final lastTodoIndex = _todos.length;
 
-      final Todo cretaedTodo = Todo(
+      final Todo createdTodo = Todo(
         id: (lastTodoIndex + 1).toString(),
         name: name,
         description: description,
         done: done,
       );
 
-      return Result.ok(cretaedTodo);
+      _todos.add(createdTodo);
+
+      await _storageService.saveTodos(_todos);
+
+      return Result.ok(createdTodo);
     } on Exception catch (e) {
       return Result.error(e);
     } finally {
@@ -36,20 +52,43 @@ class TodosRepositoryDev extends ChangeNotifier implements TodosRepository {
 
   @override
   Future<Result<void>> delete(Todo todo) async {
-    if (_todos.contains(todo)) {
-      _todos.remove(todo);
+    try {
+      _todos.removeWhere((t) => t.id == todo.id);
+
+      await _storageService.saveTodos(_todos);
+
+      return Result.ok(null);
+    } on Exception catch (e) {
+      return Result.error(e);
+    } finally {
+      notifyListeners();
     }
-    return Result.ok(null);
   }
 
   @override
   Future<Result<List<Todo>>> get() async {
-    return Result.ok(_todos);
+    try {
+      _todos = await _storageService.loadTodos();
+
+      return Result.ok(_todos);
+    } on Exception catch (e) {
+      return Result.error(e);
+    } finally {
+      notifyListeners();
+    }
   }
 
   @override
   Future<Result<Todo>> getById(String id) async {
-    return Result.ok(_todos.firstWhere((todo) => todo.id == id));
+    try {
+      final todo = _todos.firstWhere(
+        (todo) => todo.id == id,
+        orElse: () => throw Exception('Todo not found'),
+      );
+      return Result.ok(todo);
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
   }
 
   @override
@@ -57,7 +96,13 @@ class TodosRepositoryDev extends ChangeNotifier implements TodosRepository {
     try {
       final todoIndex = _todos.indexWhere((t) => t.id == todo.id);
 
+      if (todoIndex == -1) {
+        throw Exception('Todo not found');
+      }
+
       _todos[todoIndex] = todo;
+
+      await _storageService.saveTodos(_todos);
 
       return Result.ok(todo);
     } on Exception catch (e) {
